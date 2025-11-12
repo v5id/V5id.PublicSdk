effe8db8415e08a6eedcdb95804a915a3de96c49
Viacheslav Stohul <55156288+ViacheslavStohul@users.noreply.github.com>
2025-11-12 21:25:43 +0200

    Add feature build workflow and update assembly version
    Introduces a new GitHub Actions workflow for building feature branches and publishing NuGet packages. Updates the global assembly version to 1.0.0 in GlobalAssemblyInfo.cs.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Runtime.CompilerServices;
 // This shows up as Product Version in Windows Explorer
 // We make this the same for all files in a particular product version. And increment it globally for all projects.
 // We then use this as the Product Version in installers as well (for example built using Wix).
-[assembly: AssemblyInformationalVersion("0.0.1")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
