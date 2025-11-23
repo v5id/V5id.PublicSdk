fba4c776c48c15f7acd2e227831942c9656e3c4d
Viacheslav Stohul <55156288+ViacheslavStohul@users.noreply.github.com>
2025-11-23 22:15:25 +0200

    Remove unused properties from FaceDetail model

--- PublicSdk/Models/FaceDetail.cs
+++ PublicSdk/Models/FaceDetail.cs
@@ -11,12 +11,4 @@ public class FaceDetail
     public Pose? Pose { get; init; }
 
     public AgeRange? AgeRange { get; init; }
-    
-    public FacialHair? FacialHair { get; init; }
-    
-    public required string FileName { get; set; }
-
-    public required string ContainerName { get; set; }
-
-    public required string StorageType { get; set; }
 }
