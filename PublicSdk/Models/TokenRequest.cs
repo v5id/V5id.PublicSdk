7c2aea94f0c4448459c55e2680971a6b923a579e
Viacheslav Stohul <55156288+ViacheslavStohul@users.noreply.github.com>
2026-01-21 03:37:06 +0200

    Remove redundant `JsonPropertyName` attributes from `TokenRequest`.

--- PublicSdk/Models/TokenRequest.cs
+++ PublicSdk/Models/TokenRequest.cs
@@ -7,13 +7,10 @@ namespace V5iD.PublicSdk.Models
 {
     public class TokenRequest
     {
-        [JsonPropertyName("client_id")]
         public string ClientId { get; set; } = string.Empty;
 
-        [JsonPropertyName("client_secret")]
         public string ClientSecret { get; set; } = string.Empty;
 
-        [JsonPropertyName("grant_type")]
         public string GrantType { get; set; } = string.Empty;
     }
 }
