2212edf27f097695d56c1397ab471e379dccedcf
Viacheslav Stohul <55156288+ViacheslavStohul@users.noreply.github.com>
2026-02-11 11:04:36 +0200

    Update SDK versions in workflows and introduce `Person` model in `PublicSdk` (#11)

---
+++ PublicSdk/Models/Person.cs
@@ -0,0 +1,16 @@
+// © Copyright (c) V5iD, Inc. All rights reserved.
+// Licensed under the MIT.
+
+namespace V5iD.PublicSdk.Models
+{
+    public class Person
+    {
+        public string? FirstName { get; init; }
+        
+        public string? LastName { get; init; }
+        
+        public string? MiddleName { get; init; }
+        
+        public int? Age { get; init; }
+    }
+}
