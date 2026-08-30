2add9f7c0cc4465ba58c7e744166d78470c8c270
Alexander Altotsky <alex@renetusa.com>
2026-08-29 20:35:53 -0700

    Added EqualsDiacritics extension method

---
+++ PublicSdk.Tests/PublicSdk.Tests/StringExtensionFixture.cs
@@ -0,0 +1,46 @@
+﻿// © Copyright (c) V5iD, Inc. All rights reserved.
+// Licensed under the MIT.
+
+using System.Globalization;
+using V5iD.PublicSdk.Extension;
+namespace PublicSdk.Tests
+{
+    public class StringExtensionFixture
+    {
+        [Fact]
+        public void EqualsDiacritics_ShouldReturnTrue_ForEquivalentStringsWithDiacritics()
+        {
+            // Arrange
+            string str1 = "AAABBCC";
+            string str2 = "ÄÁÅḂB́ĈĊ";
+            // Act
+            bool result = str1.EqualsDiacritics(str2);
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void EqualsDiacritics_ShouldReturnFalse_ForEquivalentStringsWithDiacriticsDifferentCase()
+        {
+            // Arrange
+            string str1 = "AAABBCCC";
+            string str2 = "ÄÁÅḂB́ĈĊĉ";
+            // Act
+            bool result = str1.EqualsDiacritics(str2);
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void EqualsDiacritics_ShouldReturnTrue_ForEquivalentStringsWithDiacriticsDifferentCase()
+        {
+            // Arrange
+            string str1 = "AAABBCCC";
+            string str2 = "ÄÁÅḂB́ĈĊĉ";
+            // Act
+            bool result = str1.EqualsDiacritics(str2, CompareOptions.IgnoreCase);
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ PublicSdk/Extension/StringExtension.cs
@@ -0,0 +1,22 @@
+﻿// © Copyright (c) V5iD, Inc. All rights reserved.
+// Licensed under the MIT.
+
+using System.Globalization;
+
+namespace V5iD.PublicSdk.Extension
+{
+    public static class StringExtension
+    {
+        public static bool EqualsDiacritics(this string str, string other, CompareOptions compareOptions = CompareOptions.IgnoreNonSpace)
+        {
+            // Compare with IgnoreNonSpace flag
+            int result = CultureInfo.CurrentCulture.CompareInfo.Compare(
+                str,
+                other,
+                CompareOptions.IgnoreNonSpace | compareOptions
+            );
+
+            return result == 0;
+        }
+    }
+}
